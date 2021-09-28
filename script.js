d574fda1cad916d058c2bf5f8b1e1415ec4410cf
jacksonroque <jackson.roque@protonmail.com>
2021-09-27 23:47:58 -0300

    alteração de parâmetros
    alteração de parâmetros

--- script.js
+++ script.js
@@ -1,89 +1,83 @@
 const display = document.querySelector("#display");
 const numeros = document.querySelectorAll("[id*=tecla]");
+const operadores = document.querySelectorAll("[id*=operador]");
+const igual = document.querySelector("#igual");
 
 let novoNumero = true;
 let operador;
 let numeroAnterior;
+const atualizarDisplay = (texto) => {
 
-const atualizarDisplay = (texto) =>{
-    if (novoNumero){
-    display.textContent = texto;
-    novoNumero = false;
+    if(novoNumero){
+        display.textContent = texto;
+        novoNumero = false;
     }else{
         display.textContent += texto;
     }
-};
+        
+}
 
-const inseriNumero = (event)=>
+const inserirNumero = (event) => 
     atualizarDisplay(event.target.textContent);
 
-    numeros.forEach((numero) => numero.addEventListener("click", inseriNumero));
+numeros.forEach(numero => numero.addEventListener("click", inserirNumero));
 
+const selecionarOperador = (event) => {
+    novoNumero = true;
+    operador = event.target.textContent;
+    numeroAnterior = display.textContent.replace(",", ".");
+}
 
-    const selecionaOperador = (event) =>{
+operadores.forEach(operador => operador.addEventListener("click", selecionarOperador));
+
+const calcular = () =>{
+    if (operador !== undefined) {
+        const numeroAtual = display.textContent.replace(",", ".");
+        const total = eval(numeroAnterior + operador + numeroAtual)
         novoNumero = true;
-        operador = event.target.textContent;
-        numeroAnterior = display.textContent.replace(",",".");
-    };
-
-    operadores.forEach((operador) => 
-    operador.addEventListener("click", selecionaOperador));
-    
-
-const calcular = () => {
-    if(operador !== undefined){
-            const numeroAtual = display.textContent.replace(",",".");
-            //calculo utlizando a funcção  eval
-        const numeroAnterior = display.textContent;
-        const resultado = eval(`${numeroAnterior}${operador}${numeroAtual}`);
-        novoNumero = true;
-        atualizarDisplay(resultado.toString().replace(".",","));
-        
-        //fazer a atualização do display com o resultado
+        atualizarDisplay(total.toString().replace(".", ","));
+      
         operador = undefined;
-        }
-};
+    }
+}
 
-const ativarIgual = () => calcular();
+igual.addEventListener("click", calcular);
 
-document.querySelector("#igual").addEventListener("click",ativarIgual);
+const limparDisplay = () => display.textContent = ""; 
+document.querySelector("#limparDisplay").addEventListener("click", limparDisplay); 
 
-const limparCalculo = () => {
+const limparCalculo = () =>{
     limparDisplay();
     novoNumero = true;
     operador = undefined;
     numeroAnterior = undefined;
 }
 
-document.querySelector("#limparCalculo").addEventListener("click", limparCalculo);
+document.querySelector("#limparCalculo").addEventListener("click", limparCalculo); 
 
-const removerUltimoNumero = () => display.textContent = display.textContent.slice(0, -1);
+const backspace = () => display.textContent = display.textContent.slice(0, -1);
 
-document.querySelector("#backspace").addEventListener("click", removerUltimoNumero);
-//inverter o sinal
+document.querySelector("#backspace").addEventListener("click", backspace); 
+
 const inverterSinal = () => {
     novoNumero = true;
-    atualizarDisplay(display.textContent * -1);
+    display.textContent = display.textContent * (-1);
+}
+document.querySelector("#inverter").addEventListener("click", inverterSinal); 
+
+const existeValor = () =>  display.textContent.length > 0 ;
+
+const existDecimal = () => display.textContent.indexOf(",") !== -1;
+
+const inserirDecimal = () => {
+    if(!existDecimal()){
+        if (existeValor()){
+            atualizarDisplay(",");
+        }else{
+            atualizarDisplay("0,");
+        }
+    }
+   
 }
 
-document.querySelector("#inverter").addEventListener("click", inverterSinal);
-
-const existirValor = () => display.textContent.length > 0;
-const existeDecimal = () => display.textContent.indexOf(",") !== -1;
-
-//retorar se existe algum coisa dentro do display
-const existirValor = () => display.textContent.length > 0;
-
-//verificar se existir algum valor no display
-const inserirDecimal = () => {
-    if(!existeDecimal()){
-        if(existeValor()){
-            atualizarDisplay(",");
-        }
-    }else{
-        atualizarDisplay("0,");
-    }
-};
-
 document.querySelector("#decimal").addEventListener("click", inserirDecimal);
-
